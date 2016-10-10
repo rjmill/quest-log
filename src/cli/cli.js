386f7b0b892d80ee4b6dfac502b1e5e28639c417
RJ Miller <rjmiller10@gmail.com>
2016-10-10 13:22:40 -0400

    start implementing cli
    Implemented rudimentary "new" and "show" commands.

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,1 +1,35 @@
 var vorpal = require('vorpal')();
+var quest = require('../quest/quest.js');
+
+// TODO: keep global state elsewhere
+var globalQuestList = require('../quest/questList.js')();
+
+vorpal.mode('new', 'new quest')
+  .alias('n')
+  .delimiter('newQuest:')
+  .init(function(args, callback) {
+    this.log('You can now start entering new quests.\nType "exit" to leave this mode.');
+    callback();
+  })
+  .action(function(command, callback) {
+    globalQuestList.addQuest(quest(command));
+    this.log('quest added');
+    callback();
+  })
+
+vorpal.command('show', 'show global quests')
+  .alias('s')
+  .action(function(args, callback) {
+    var self = this;
+    globalQuestList
+      .getQuestList()
+      .forEach(function(qst) {
+        self.log(qst.getDescription());
+      });
+    callback();
+  })
+
+// TODO: look into getting a cooler delimiter
+vorpal
+  .delimiter('qlog$')
+  .show();
