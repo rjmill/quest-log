7b79c6c566907f40371ec33d7ff5fbdb59b94373
RJ Miller <rjmiller10@gmail.com>
2016-11-07 21:51:56 -0500

    Move things out of redundant IIFEs

--- src/quest/counterId.js
+++ src/quest/counterId.js
@@ -2,22 +2,20 @@
  * Stupid simple generation of unique IDs. Will replace with something more
  * sensible when it makes sense todo so
  */
-var counterId = (function() {
-  var STARTING_COUNTER = 0;
-  var counter = STARTING_COUNTER;
 
-  var createNewId = function() {
-    counter++;
-    return counter;
-  }
-  var resetIds = function() {
-    counter = STARTING_COUNTER;
-  }
+var STARTING_COUNTER = 0;
+var counter = STARTING_COUNTER;
 
-  return {
-    createNewId: createNewId,
-    resetIds: resetIds,
-  }
-})();
+var createNewId = function() {
+  counter++;
+  return counter;
+}
 
-module.exports = counterId;
+var resetIds = function() {
+  counter = STARTING_COUNTER;
+}
+
+module.exports = {
+  createNewId: createNewId,
+  resetIds: resetIds,
+};
--- src/quest/questStatus.js
+++ src/quest/questStatus.js
@@ -1,31 +1,28 @@
 // TODO: since you're using require, you can make this easier to read
-var questStatus = (function() {
-  var POSSIBLE_STATUSES = [
-    "in progress",
-    "completed",
-  ];
-  var DEFAULT_STATUS = "in progress";
+var POSSIBLE_STATUSES = [
+  "in progress",
+  "completed",
+];
+var DEFAULT_STATUS = "in progress";
 
-  var isValidStatus = function(status) {
-    return POSSIBLE_STATUSES.indexOf(status) !== -1;
+var isValidStatus = function(status) {
+  return POSSIBLE_STATUSES.indexOf(status) !== -1;
+};
+
+function questStatus() {
+  var status = DEFAULT_STATUS;
+  return {
+    getStatus: function() {
+      return status;
+    },
+    setStatus: function(newStatus) {
+      if (!isValidStatus(newStatus)) {
+        throw new Error("invalid status");
+      }
+      status = newStatus;
+      return this;
+    },
   };
-
-  function questStatus() {
-    var status = DEFAULT_STATUS;
-    return {
-      getStatus: function() {
-        return status;
-      },
-      setStatus: function(newStatus) {
-        if (!isValidStatus(newStatus)) {
-          throw new Error("invalid status");
-        }
-        status = newStatus;
-        return this;
-      },
-    };
-  }
-  return questStatus;
-})();
+}
 
 module.exports = questStatus;
